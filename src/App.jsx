3b05784e62a95e4c84d2237f85b978c0604c3db2
Baxa-1998 <baxtiyornarziev6@gmail.com>
2025-07-26 16:35:41 +0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.scss'
+import { Footer } from './components/Footer/Footer'
+import { Header } from './components/Header/Header'
+import { Hero } from './components/Hero/Hero'
+
+function App() {
+
+
+  return (
+    <div className='wrapper'>
+      <Header/> 
+      <Hero/> 
+    <Footer/>
+
+  
+    </div>
+  )
+}
+
+export default App
