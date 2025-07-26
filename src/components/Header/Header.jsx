e6852a20cc001438785b071bc0afacbd3426798b
Baxa-1998 <baxtiyornarziev6@gmail.com>
2025-07-26 17:17:28 +0500

    first-commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import './header.scss'
 export const Header = () => {
   return (
-    <header>Header</header>
+    <header className='header'>
+      <div className='header__wrapper'> 
+      <ul>
+        <li>
+          <a href="#collection">Collection</a>
+          <a href="#collection">About Us</a>
+        </li>
+      </ul>
+      </div>
+    </header>
   )
 }
